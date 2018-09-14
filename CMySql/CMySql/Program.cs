f86edb413876eea45204aab4b888d550179277ca
tsvetos <seve0955@gmail.com>
2018-09-14 09:19:22 +0200

    Añadido MySql.Dara.dll CholaMundo y CMySql

---
+++ CMySql/CMySql/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CMySql
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			MySqlConnection mySqlConnection = new MySqlConnection (
+				"server=localhost;database=dbprueba;user=root;password=sistemas;sslmode=none"
+			);
+			mySqlConnection.Open();
+			mySqlConnection.Close();
+		}
+	}
+}
