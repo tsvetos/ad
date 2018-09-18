46edfcead67990ed7b2eb937c33135a70869bfd6
tsvetos <seve0955@gmail.com>
2018-09-18 09:55:38 +0200

    Actualizado CMySql con metodo nuevo

---
+++ CCategoria/CCategoria/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+		treeView.AppendColumn("ID", new CellRendererText(), "text", 0);
+		treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+
+		ListStore listStore = new ListStore(typeof(string), typeof(string));
+		treeView.Model = listStore;
+		listStore.AppendValues("1", "cat 1");
+		listStore.AppendValues("2", "cat 2");
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Collections.Generic;
 
 namespace CMySql
 {
 	class MainClass
 	{
-		private static string[] getFieldNames(){
-			throw new NotImplementedException();	
+		private static string[] getFieldNames(IDataReader dataReader ){
+			int fieldCount = dataReader.FieldCount;
+			//string[] fieldNames = new string[fieldCount];
+			//for (int index = 0; index < fieldCount; index++)
+			//fieldNames[index] = dataReader.GetName(index);
+			//	return fieldNames;
+			List<String> fieldNameList = new List<string>();
+			for (int index = 0; index < fieldCount; index++)
+				fieldNameList.Add(dataReader.GetName(index));
+			//fieldNameList.Add("id");
+			//fieldNameList.Add("nombre");
+			return fieldNameList.ToArray();
+				
+			
 		}
 		public static void Main (string[] args)
 		{
@@ -17,10 +30,18 @@ namespace CMySql
 			dbConnection.Open();
 
 			IDbCommand dbCommand = dbConnection.CreateCommand();
-			dbCommand.CommandText = "select * from categoria order by id";
+			dbCommand.CommandText = "select id, nombre,'dato fijo' from categoria order by id";
 			IDataReader dataReader = dbCommand.ExecuteReader();
-			while (dataReader.Read())
-				Console.WriteLine("id = '{0}'|nombre = '{1}' ", dataReader["id"], dataReader["nombre"]);
+			string[] fieldNames = getFieldNames(dataReader);
+			foreach (string fieldName in fieldNames)
+				Console.WriteLine("Columna=" + fieldName);
+
+
+			//for (int index = 0; index < dataReader.FieldCount; index++)
+			//	Console.WriteLine("Columna {0} = {1} ", index, dataReader.GetName(index));
+
+			//while (dataReader.Read())
+			//	Console.WriteLine("id = '{0}'|nombre = '{1}' ", dataReader["id"], dataReader["nombre"]);
 			dbConnection.Close();
 
 
