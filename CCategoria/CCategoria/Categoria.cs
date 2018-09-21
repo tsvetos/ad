2662e65bcbd05cfc338c9cf5543461942171d456
tsvetos <seve0955@gmail.com>
2018-09-21 09:10:26 +0200

    Añadido CCategoria

---
+++ CCategoria/CCategoria/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CCategoria
+{
+    public class Categoria
+    {
+		private ulong id;
+		private string nombre;
+
+        public Categoria()
+        {
+        }
+		public Categoria(ulong id, String nombre){
+			this.id = id;
+			this.nombre = nombre;
+		}
+		public ulong Id { 
+			get { return Id; }
+			set { id = value; }
+		}
+		public string Nombre { 
+			get { return nombre; }
+			set { nombre = value; }
+		}
+    }
+}
--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -1,21 +1,43 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Collections.Generic;
 using Gtk;
 
 public partial class MainWindow : Gtk.Window
 {
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
-        Build();
+		Build();
+
+
+
+		IDbConnection dbConnection = new MySqlConnection(
+                "server=localhost;database=dbprueba;user=root;password=sistemas;sslmode=none"
+            );
+        dbConnection.Open();
 
 		treeView.AppendColumn("ID", new CellRendererText(), "text", 0);
 		treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
 
-		ListStore listStore = new ListStore(typeof(string), typeof(string));
+	//	ListStore listStore = new ListStore(typeof(ulong), typeof(string)); //ulong acepta el formato del id
+		ListStore listStore = new ListStore(typeof(string), typeof(string)); //con string hay que usar ToString()
 		treeView.Model = listStore;
-		listStore.AppendValues("1", "cat 1");
-		listStore.AppendValues("2", "cat 2");
-    }
+        
 
+		//listStore.AppendValues("1", "cat 1");
+		//listStore.AppendValues("2", "cat 2");
+
+		IDbCommand dbCommand = dbConnection.CreateCommand();
+		dbCommand.CommandText = "select id, nombre from categoria order by id";
+		IDataReader dataReader = dbCommand.ExecuteReader();
+		while (dataReader.Read())
+			listStore.AppendValues(dataReader["id"].ToString(), dataReader["nombre"].ToString());
+
+
+		dbConnection.Close();
+    }
+    
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
         Application.Quit();
