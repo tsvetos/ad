33a523b8a1190b4ddcd43e8d92b43e005223bc25
tsvetos <seve0955@gmail.com>
2019-01-23 12:12:15 +0100

    dbprueba.sql: añadidas cliente, pedido, pedidolinea

--- dbprueba.sql
+++ dbprueba.sql
@@ -14,9 +14,39 @@ create table articulo (
  categoria bigint unsigned
 );
 
-alter table articulo add foreig nkey (categoria) references categoria (id);
+alter table articulo add foreign key (categoria) references categoria (id);
 
-insert into articulo (nombre, precio, categoria) values ('articulo 1',1.0,1);
-insert into articulo (nombre, precio, categoria) values ('articulo 2',2.0,2);
-insert into articulo (nombre, precio, categoria) values ('articulo 3',3.0,3);
-insert into articulo (nombre, precio) values ('articulo 4',4.0);
+insert into articulo (nombre, precio, categoria) values ('artículo 1', 1.0, 1);
+insert into articulo (nombre, precio, categoria) values ('artículo 2', 2.0, 2);
+insert into articulo (nombre, precio, categoria) values ('artículo 3', 3.0, 3);
+insert into articulo (nombre, precio) values ('artículo 4', 4.0);
+
+create table cliente (
+ id serial primary key,
+ nombre varchar(50) not null unique
+);
+
+insert into cliente (nombre) values ('cliente 1');
+insert into cliente (nombre) values ('cliente 2');
+insert into cliente (nombre) values ('cliente 3');
+
+create table pedido (
+ id serial primary key,
+ fecha datetime not null unique,
+ cliente bigint unsigned,
+ importe numeric(10,2) not null
+);
+
+alter table pedido add foreign key (cliente) references cliente (id);
+
+create table pedidolinea (
+ id serial primary key,
+ pedido bigint unsigned not null,
+ articulo bigint unsigned not null,
+ precio numeric(10,2) not null,
+ unidades numeric(10,2) not null,
+ importe numeric(10,2) not null
+);
+
+alter table pedidolinea add foreign key (pedido) references pedido (id) on delete cascade;
+alter table pedidolinea add foreign key (articulo) references articulo (id);
